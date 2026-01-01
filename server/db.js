a7f8f27f9c1954b1ede2adce123bcb98533cca9e
Chandan03590 <ckc03590@gmail.com>
2026-01-01 15:24:08 +0530

    correct appname in db.js

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose')
 
 async function connectMongo(){
     try{
-      await mongoose.connect("mongodb+srv://ecommerce:MmB3Lmca6ywHKcpt@ecommercecluster.s7kjtqz.mongodb.net/Esallonservices?apcpName=EcommerceCluster"),{
+      await mongoose.connect("mongodb+srv://ecommerce:MmB3Lmca6ywHKcpt@ecommercecluster.s7kjtqz.mongodb.net/Esallonservices?appName=EcommerceCluster"),{
         useNewUrlParser:true,
         useUnifiedTopology:true
       }
